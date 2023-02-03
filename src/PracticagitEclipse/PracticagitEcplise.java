3a10263eaeb2cfbc3c3538f197c432f297a9bf92
julio <julioberu41@gmail.com>
2023-02-03 15:54:47 +0100

    guardando el progreso

---
+++ src/PracticagitEclipse/PracticagitEcplise.java
@@ -0,0 +1,22 @@
+package PracticagitEclipse;
+
+import java.util.Scanner;
+
+public class PracticagitEcplise {
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		      System.out.println("dime un numero: ");
+		      int a = sc.nextInt();
+		    
+		      System.out.println("dime un numero: ");
+		      int b = sc.nextInt();
+		      
+		      boolean resultado = (a == b);
+		      
+		      System.out.println("El resultado es: " + resultado);
+		      sc.close();
+			}
+}
+
